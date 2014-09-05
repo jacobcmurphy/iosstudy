7c50f96ede3ed839e4b5ce7aac9cd2358bbc1296
Samir Undavia <Samir@Samirs-MacBook-Air.local>
2014-09-04 23:03:29 -0400

    Location Test

--- wgo/wgo/FirstViewController.swift
+++ wgo/wgo/FirstViewController.swift
@@ -8,20 +8,47 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
-                            
-    @IBOutlet var mapView: MKMapView!
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+      @IBOutlet var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+  //  func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
+
+
+
 
