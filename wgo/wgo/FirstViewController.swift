d8a295bf121ff1dd7fc92a697a3520e74c0ad08d
Sharat Sridhar <sharat@Sharats-MacBook-Pro.local>
2014-09-04 21:23:30 -0400

    map added

--- wgo/wgo/FirstViewController.swift
+++ wgo/wgo/FirstViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
                             
+    @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
