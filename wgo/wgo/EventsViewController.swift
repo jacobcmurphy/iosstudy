9ad7c98807e1fdc842b55ef1efd1b7810d83351c
David Giliotti <davidgil@brandeis.edu>
2014-11-11 18:44:29 -0500

    Logos -Help From Samir

--- wgo/wgo/EventsViewController.swift
+++ wgo/wgo/EventsViewController.swift
@@ -14,8 +14,6 @@ import Foundation
 class EventsViewController: UIViewController, UITableViewDelegate, NSXMLParserDelegate{
     
     
-    
-    
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
