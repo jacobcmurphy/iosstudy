c033c0aed08dfb99c6f68367953ed1ef57510726
David Giliotti <davidgil@brandeis.edu>
2014-11-12 15:57:53 -0500

    Test

---
+++ wgo/wgo/Constants.swift
@@ -0,0 +1,57 @@
+//
+//  Constants.swift
+//  OAuthSwift
+//
+//  Created by Dongri Jin on 7/17/14.
+//  Copyright (c) 2014 Dongri Jin. All rights reserved.
+//
+
+import Foundation
+
+let Twitter =
+[
+    "consumerKey": "Dt3eXW85b4Enalxg2tyhH07mw",
+    "consumerSecret": "llzxOj48HnMKzJ4ucs8wv8FqRLgKui0myS8Obb2529JsydSVUd"
+]
+
+let Google =
+[
+    "consumerKey": "932148694004-2009ffast4c8br9mcj4al5g1qdp6qcja.apps.googleusercontent.com",
+    "consumerSecret": "deUyf2badsmkeeGvVSI5Lofw"
+]
+
+let Flickr =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
+let Github =
+[
+    "consumerKey": "e99a0c4c085a05117a45",
+    "consumerSecret": "5c8c2706f279abcc618b9263d8119bf4d0fd74ae"
+]
+let Instagram =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
+let Foursquare =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
+let Fitbit =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
+let Withings =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
+let Linkedin =
+[
+    "consumerKey": "***",
+    "consumerSecret": "***"
+]
--- wgo/wgo/LoginController.swift
+++ wgo/wgo/LoginController.swift
@@ -21,6 +21,8 @@ class LoginController: UIViewController {
     @IBOutlet weak var emailVar: UITextField!
     @IBOutlet weak var passVar: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var githubLogin: UIButton!
+
     var user = User()
     
     
@@ -43,6 +45,7 @@ class LoginController: UIViewController {
         if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [UserEn] {
             if(fetchResults.isEmpty){
                 loginButton.addTarget(self, action: Selector("loginClick"), forControlEvents: .TouchUpInside)
+                githubLogin.addTarget(self, action: Selector("doOAuthGithub"), forControlEvents: .TouchUpInside)
             } else {
                 let firstViewController = self.storyboard?.instantiateViewControllerWithIdentifier("FirstViewController") as UIViewController
             }
@@ -54,31 +57,101 @@ class LoginController: UIViewController {
     @IBAction func sendLogin(sender: AnyObject) {
     }
     
-    func loginClick(){
+
+    func loginClick() {
+//        
+        loginButton.hidden = true;
+          activityMon.hidden = false;
+          activityMon.hidesWhenStopped = true
+         // activityMon.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+          view.addSubview(activityMon)
+          activityMon.startAnimating()
+          let newItem = NSEntityDescription.insertNewObjectForEntityForName("UserEn", inManagedObjectContext: self.managedObjectContext!) as UserEn
+        
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil);
+        let vc = storyboard.instantiateViewControllerWithIdentifier("tabViewController") as UITabBarController;
+        let oauthswift = OAuth1Swift(
+            consumerKey:    Twitter["consumerKey"]!,
+            consumerSecret: Twitter["consumerSecret"]!,
+            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+        )
+        
+        oauthswift.authorizeWithCallbackURL( NSURL(string: "wgo://oauth-callback/twitter")!, success: {
+            credential, response in
+            self.presentViewController(vc, animated: false, completion: nil);
+
+            println("Twitter", message: "auth_token:\(credential.oauth_token)\n\noauth_toke_secret:\(credential.oauth_token_secret)\n")
+            var request = HTTPTask()
+            request.responseSerializer = JSONResponseSerializer()
+            request.baseURL = "http://leiner.cs-i.brandeis.edu:6000"
+            request.POST("/twitterauth", parameters: ["twittertoken": "\(credential.oauth_token)" ], success: {(response: HTTPResponse) -> Void in
+                let data = response.responseObject as NSDictionary
+                newItem.id = data.valueForKey("_id") as String
+                newItem.first_name = data.valueForKey("first_name") as String
+                newItem.last_name = data.valueForKey("last_name") as String
+                self.user.loc = data.valueForKey("loc") as Array<Double>
+                newItem.long = self.user.loc[0]
+                newItem.lat = self.user.loc[1]
+                },failure: {(error: NSError) -> Void in
+            })
+
+            var parameters =  Dictionary<String, AnyObject>()
+        
+            }, failure: {(error:NSError!) -> Void in
+                println("Failed to login")
+                println(error.localizedDescription)
+        })
+        
+
+    }
+    
+    func doOAuthGithub(){
+        
         loginButton.hidden = true;
         activityMon.hidden = false;
         activityMon.hidesWhenStopped = true
-       // activityMon.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        // activityMon.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
         view.addSubview(activityMon)
         activityMon.startAnimating()
         let newItem = NSEntityDescription.insertNewObjectForEntityForName("UserEn", inManagedObjectContext: self.managedObjectContext!) as UserEn
-        var request = HTTPTask()
-        request.auth = HTTPAuth(username: emailVar.text, password: passVar.text)
-        request.auth?.persistence = .Permanent
-        request.responseSerializer = JSONResponseSerializer()
-        request.baseURL = "http://leiner.cs-i.brandeis.edu:6000"
-        request.GET("/login/\(emailVar.text)/\(passVar.text)", parameters: nil, success: {(response: HTTPResponse) -> Void in
-            let data = response.responseObject as NSDictionary
-            newItem.id = data.valueForKey("_id") as String
-            newItem.first_name = data.valueForKey("first_name") as String
-            newItem.last_name = data.valueForKey("last_name") as String
-            self.user.loc = data.valueForKey("loc") as Array<Double>
-            newItem.long = self.user.loc[0]
-            newItem.lat = self.user.loc[1]
-            },failure: {(error: NSError) -> Void in
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil);
+        let vc = storyboard.instantiateViewControllerWithIdentifier("firstViewController") as UIViewController;
+        
+        let oauthswift = OAuth2Swift(
+            consumerKey:    Github["consumerKey"]!,
+            consumerSecret: Github["consumerSecret"]!,
+            authorizeUrl:   "https://github.com/login/oauth/authorize",
+            accessTokenUrl: "https://github.com/login/oauth/access_token",
+            responseType:   "code"
+        )
+        oauthswift.authorizeWithCallbackURL( NSURL(string: "wgo://oauth-callback/github")!, scope: "user,repo", state: "GITHUB", success: {
+            credential, response in
+            self.presentViewController(vc, animated: false, completion: nil);
+            println("Github", message: "oauth_token:\(credential.oauth_token)")
+            var request = HTTPTask()
+            request.responseSerializer = JSONResponseSerializer()
+            request.baseURL = "http://leiner.cs-i.brandeis.edu:6000"
+            request.POST("/twitterauth", parameters: ["twittertoken": "\(credential.oauth_token)" ], success: {(response: HTTPResponse) -> Void in
+                let data = response.responseObject as NSDictionary
+                newItem.id = data.valueForKey("_id") as String
+                newItem.first_name = data.valueForKey("first_name") as String
+                newItem.last_name = data.valueForKey("last_name") as String
+                self.user.loc = data.valueForKey("loc") as Array<Double>
+                newItem.long = self.user.loc[0]
+                newItem.lat = self.user.loc[1]
+                },failure: {(error: NSError) -> Void in
+            })
+
+            }, failure: {(error:NSError!) -> Void in
+                println(error.localizedDescription)
         })
         
     }
+    
         override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
